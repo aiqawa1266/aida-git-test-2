33d49b0101d1ff134c026c200ea3f9e184b7f52a
aiqawa1266 <aiqawachan@gmail.com>
2025-07-18 09:56:21 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
   <h1>Hello World!</h1>
   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, voluptate exercitationem ipsam maiores rem quas debitis. Dicta, aliquid. Placeat dolor, id alias saepe quas eum eius nobis accusamus tempora ex?</p>
+  <ul>
+    <li>satu</li>
+    <li>Dua</li>
+  </ul>
 </body>
 </html>
